04a8492a22f8012e186f6a0e0983582557cc0605
viniciussm07 <vini.soares.martins@gmail.com>
2025-04-29 21:07:03 -0300

    Aula 10

---
+++ aulas/Aula10/teste.c
@@ -0,0 +1,17 @@
+#include "adjlist.h"
+
+int main () {
+    int n, x, y, z;
+
+    Graph myGraph;
+
+    if (scanf("%d", &n) == 1){
+        myGraph = initGraph(n);
+        while(scanf("%d %d %d", &x, &y, &z) != EOF) {
+            insertArc(myGraph, x, y, z); // Adicionado o peso (z)
+        }
+        printGraph(myGraph);
+        floydWarshall(myGraph);
+        destroyGraph(myGraph);
+    }
+}
